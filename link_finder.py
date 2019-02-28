b5a4d7a126efa6128a387e8a4587a31310eaa74c
Ian Campbell <ixc9335@rit.edu>
2019-02-28 18:17:14 -0500

    Setup options and basic functions, setup to dictate foreign addresses

--- link_finder.py
+++ link_finder.py
@@ -7,55 +7,106 @@
 # version 1.0.0 02/28/19
 
 import getopt
-import re
+import sys
 from bs4 import BeautifulSoup
 import requests
 import requests.exceptions
 from urllib.parse import urlsplit
 from collections import deque
 
-#opening message
-print('Welcome to link_finder.py\n')
 
-#hardcoded website
-start = "http://krukafdafampbell.com/"
-
-# data structures
-new_urls = deque([start])
+foreign_urls = set()
 processed_urls = set()
 
-#main while loop
-while len(new_urls):
-    url = new_urls.popleft()
-    processed_urls.add(url)
-
-    parts = urlsplit(url)
-    #setup url
-    base_url = "{0.scheme}://{0.netloc}".format(parts)
-    if parts.scheme != 'mailto' and parts.scheme != '#':
-        path = url[:url.rfind('/') + 1] if '/' in parts.path else url
-    else:
-        continue
-    
-    #determine if it works or is broken
+def main():
     try:
-        response = requests.get(url)
-        print (url + " is alive")
-    except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL):
-        print( url + " is Broken\n")
-        continue
+        opts,args = getopt.getopt(sys.argv[1:], "o:l:mh", ["help", "ouput=", "limit="])
+    except getopt.GetoptError as err:
+        print(err)
+        usage()
+        sys.exit(2)
+    outfile = None
+    working = False
+    limits = None 
+    for one, two in opts:
+        if one == "-m":
+            working = True
+        elif one in ("-h", "--help"):
+            usage()
+            sys.exit()
+        elif one in ("-o","--output"):
+            outfile = two
+        elif one in ("-l","--limit"):
+            limits = two
+        else:
+            assert False, "Option not permitted"
 
-    #create a BSOBJ to scan the html for links
-    soup = BeautifulSoup(response.text, 'lxml')
+    spider()
 
-    #determine which lnks are valid from the html
-    for anchor in soup.find_all("a"):
-        link = anchor.attrs["href"] if "href" in anchor.attrs and anchor.attrs["href"].find("mailto") == -1 and anchor.attrs["href"].find("tel") == -1 and anchor.attrs["href"].find("#") == -1 else ''
-        #append good links back to the url queue. 
-        if link.startswith('/'):
-            link = base_url + link
-        elif not link.startswith('http'):
-            link = path + link
-        if not link in new_urls and not link in processed_urls and not link.find(start) == -1:
-            new_urls.append(link)
 
+    print("\nFOREIGN URLS FOUND\n")
+    count = 0
+    for a in foreign_urls:
+        print(str(count) + ") " + a)
+        count = count + 1
+
+### This method contains all main functionality
+### This will spider throught the site and map out links as
+### active or broken!!
+def spider():
+
+    #opening message
+    print('Welcome to link_finder.py\n')
+
+    #hardcoded website
+    start = "http://krukandcampbell.com/"
+
+    # data structures
+    new_urls = deque([start])
+
+    #main while loop
+    while len(new_urls):
+        url = new_urls.popleft()
+        processed_urls.add(url)
+
+        parts = urlsplit(url)
+        #setup url
+        base_url = "{0.scheme}://{0.netloc}".format(parts)
+        if parts.scheme != 'mailto' and parts.scheme != '#':
+            path = url[:url.rfind('/') + 1] if '/' in parts.path else url
+        else:
+            continue
+        
+        #determine if it works or is broken
+        try:
+            response = requests.get(url)
+            print (url + " is alive")
+        except (requests.exceptions.MissingSchema, requests.exceptions.ConnectionError, requests.exceptions.InvalidURL):
+            print( url + " is Broken\n")
+            continue
+
+        #create a BSOBJ to scan the html for links
+        soup = BeautifulSoup(response.text, 'lxml')
+
+        #determine which lnks are valid from the html
+        for anchor in soup.find_all("a"):
+            link = anchor.attrs["href"] if "href" in anchor.attrs and anchor.attrs["href"].find("mailto") == -1 and anchor.attrs["href"].find("tel") == -1 and anchor.attrs["href"].find("#") == -1 else ''
+
+            #append good links back to the url queue. 
+            if link.startswith('/'):
+                link = base_url + link
+            elif not link.startswith('http'):
+                link = path + link
+            if not link in new_urls and not link in processed_urls and not link.find(start) == -1:
+                new_urls.append(link)
+            if link.find(start) == -1:
+                foreign_urls.add(link[:75])
+
+def usage():
+    print("USAGE: python3 link_finder.py -o <outfile> -m -l <domain to be limi"
+            +"ted too>\n")
+    sys.exit(2)
+
+
+if __name__ == "__main__":
+    main()
